b0304df911a826ece29235f02dfe81397bea478b
Trong Huu <huudt.3012@gmail.com>
2016-05-23 11:02:51 +0700

    upload first

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,35 @@
+<script lang="javascript">(function() {var pname = document.title;var ga = document.createElement('script'); ga.type = 'text/javascript';ga.src = '//live.vnpgroup.net/js/web_client_box.php?hash=5378c8331d02b8d134d539fe2cc9a220&data=eyJzc29faWQiOjMyMTczMiwiaGFzaCI6IjhiZjE2YjQ5NTc5NDliM2YwNDA3NGM0OTU2OGQyMTYzIn0-&pname='+pname;var s = document.getElementsByTagName('script');s[0].parentNode.insertBefore(ga, s[0]);})();</script><noscript><a href="http://www.vatgia.com" title="vatgia.com" target="_blank">Tài trợ bởi vatgia.com</a></noscript><noscript><a href="http://vchat.vn" title="vchat.vn" target="_blank">Phát triển bởi vchat.vn</a></noscript><noscript><a href="http://123doc.org" title="123doc.org" target="_blank">Partner vchat.vn</a></noscript>	
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h3 class="esp">ESP ENGLISH</h3>
+				<p>100% NATIVE ENGLISH TEACHERS</p>
+			</div>
+			
+			<div class="col-md-3 contact_all">
+				
+				
+				<p><i class="fa fa-envelope"></i> espenglish.vn@gmail.com</p>
+				<p><i class="fa fa-phone"></i> Call us: 0988331890</p>
+				
+
+			</div>		
+		</div>
+	</div>
+	<div id="social">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<p>©2015 ESP ENGLISH - BEYOND YOUR LIMITS! </p>
+				</div>
+				<div class="col-md-6">					
+					<img src="{{Asset('public/images/instagram.png')}}" class="footer_icon" >
+					<img src="{{Asset('public/images/google.png')}}" class="footer_icon" >
+					<img src="{{Asset('public/images/twitter.png')}}" class="footer_icon">
+					<img src="{{Asset('public/images/face.png')}}" class="footer_icon">					
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+@section('content')
+
+<!-- 
+<textarea id="editor1" class="ckeditor"></textarea>  
+  
+  <script type="text/javascript">  
+     CKEDITOR.replace( 'editor1' );  
+  </script>  
+-->
+@stop
